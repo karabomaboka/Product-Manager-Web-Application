db5ea2e6d0c08bbca9e17e9a0aab528b92151f99
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-04-19 22:00:23 +0200

    Add files via upload

--- add_product.php
+++ add_product.php
@@ -1,6 +1,7 @@
 <?php
 // Get the product data
-$category_id = filter_input(INPUT_POST, 'category_id', FILTER_VALIDATE_INT);
+$category_id = filter_input(INPUT_POST, 'category_id', 
+        FILTER_VALIDATE_INT);
 $code = filter_input(INPUT_POST, 'code');
 $name = filter_input(INPUT_POST, 'name');
 $price = filter_input(INPUT_POST, 'price', FILTER_VALIDATE_FLOAT);
@@ -13,11 +14,11 @@ if ($category_id == null || $category_id == false ||
 } else {
     require_once('database.php');
 
-    // Add the product to the database  
-    $query = 'INSERT INTO products
+    // Add the product to the database 
+    $query = "INSERT INTO products
                  (categoryID, productCode, productName, listPrice)
               VALUES
-                 (:category_id, :code, :name, :price)';
+                 (:category_id, :code, :name, :price)";
     $statement = $db->prepare($query);
     $statement->bindValue(':category_id', $category_id);
     $statement->bindValue(':code', $code);
