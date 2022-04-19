f4ca11d1f18b688614478423726877d6b461b9c4
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-04-19 21:59:16 +0200

    Add files via upload

--- delete_product.php
+++ delete_product.php
@@ -7,13 +7,14 @@ $category_id = filter_input(INPUT_POST, 'category_id', FILTER_VALIDATE_INT);
 
 // Delete the product from the database
 if ($product_id != false && $category_id != false) {
-    $query = 'DELETE FROM products
-              WHERE productID = :product_id';
+    $query = "DELETE FROM products
+              WHERE productID = :product_id";
     $statement = $db->prepare($query);
     $statement->bindValue(':product_id', $product_id);
-    $success = $statement->execute();
-    $statement->closeCursor();    
+    $statement->execute();
+    $statement->closeCursor();
 }
 
-// Display the Product List page
+// display the Product List page
 include('index.php');
+?>
