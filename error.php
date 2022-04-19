378dd456f7991c22231114a55b94c523b2414c64
Karabo Maboka <51972141+karabomaboka@users.noreply.github.com>
2022-04-19 21:58:39 +0200

    Add files via upload

--- error.php
+++ error.php
@@ -4,7 +4,7 @@
 <!-- the head section -->
 <head>
     <title>My Guitar Shop</title>
-    <link rel="stylesheet" type="text/css" href="main.css" />
+    <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 
 <!-- the body section -->
